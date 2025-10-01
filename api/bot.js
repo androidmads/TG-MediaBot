f248670b4bbfb8adde6424eb9ea7a9056b7af89e
androidmads <mushtaqat3gb@gmail.com>
2025-10-01 11:07:18 +0530

    fix: bot.js new link

--- api/bot.js
+++ api/bot.js
@@ -154,7 +154,6 @@ module.exports = async (req, res) => {
     bot.processUpdate(req.body);
     res.status(200).send('ok');
   } else {
-    bot.processUpdate(req.body);
     res.status(200).send('Bot is running1...');
   }
 };
